067c135ff7786d3a9599fb3b65f6aa862dc5aa77
Tomas Laurenzo <tom@laptop-inco-09.fing.edu.uy>
2014-05-08 10:16:36 -0300

    #ifdef

---
+++ src/DollHead.cpp
@@ -0,0 +1,9 @@
+//
+//  DollHead.cpp
+//  ax12OSCServer
+//
+//  Created by Tomas Laurenzo on 5/6/14.
+//
+//
+
+#include "DollHead.h"
---
+++ src/DollHead.h
@@ -0,0 +1,14 @@
+//
+//  DollHead.h
+//  ax12OSCServer
+//
+//  Created by Tomas Laurenzo on 5/6/14.
+//
+//
+
+#ifndef __ax12OSCServer__DollHead__
+#define __ax12OSCServer__DollHead__
+
+#include <iostream>
+
+#endif /* defined(__ax12OSCServer__DollHead__) */
